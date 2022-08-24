371d42918011d53b86cd1a057c7afcd68c23d20a
Matheus Dias <matheusdyas007@hotmail.com>
2022-08-23 21:19:49 -0300

    finalizando logica do cadastro

--- nestjs-api/src/auth/auth.service.ts
+++ nestjs-api/src/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -10,7 +12,27 @@ export class AuthService {
     return { msg: 'Signed in' };
   }
 
-  signup(dto: AuthDto) {
-    return { msg: dto };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 }
